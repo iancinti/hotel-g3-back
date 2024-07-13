8062b468a1c44dae4ce45cb5bb9262e78885f52b
Ian Cinti <ian.cinti@gmail.com>
2024-07-12 21:37:04 -0300

    initial commit

---
+++ src/main/java/com/g3/hotel_g3_back/HotelG3BackApplication.java
@@ -0,0 +1,13 @@
+package com.g3.hotel_g3_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelG3BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelG3BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g3/hotel_g3_back/HotelG3BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g3.hotel_g3_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelG3BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
