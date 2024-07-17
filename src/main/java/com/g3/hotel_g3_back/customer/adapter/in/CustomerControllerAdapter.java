d9d02b08e734de5f017c4d7ff0c9346d8a19c06f
Ian Cinti <ian.cinti@gmail.com>
2024-07-17 00:46:52 -0300

    added RetriveCustomerById

--- src/main/java/com/g3/hotel_g3_back/customer/adapter/in/CustomerControllerAdapter.java
+++ src/main/java/com/g3/hotel_g3_back/customer/adapter/in/CustomerControllerAdapter.java
@@ -1,5 +1,6 @@
 package com.g3.hotel_g3_back.customer.adapter.in;
 
+import com.g3.hotel_g3_back.customer.application.port.in.RetriveCustomerByIdQuery;
 import com.g3.hotel_g3_back.customer.application.port.in.RetriveCustomerQuery;
 import com.g3.hotel_g3_back.customer.domain.Customer;
 import org.slf4j.Logger;
@@ -7,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +19,13 @@ import java.util.List;
 public class CustomerControllerAdapter {
 
     private final Logger log = LoggerFactory.getLogger(CustomerControllerAdapter.class);
-    private final RetriveCustomerQuery retriveCustomerQuery;
 
-    public CustomerControllerAdapter(RetriveCustomerQuery retriveCustomerQuery) {
+    private final RetriveCustomerQuery retriveCustomerQuery;
+    private final RetriveCustomerByIdQuery retriveCustomerByIdQuery;
+
+    public CustomerControllerAdapter(RetriveCustomerQuery retriveCustomerQuery, RetriveCustomerByIdQuery retriveCustomerByIdQuery) {
         this.retriveCustomerQuery = retriveCustomerQuery;
+        this.retriveCustomerByIdQuery = retriveCustomerByIdQuery;
     }
 
     @GetMapping()
@@ -30,4 +35,13 @@ public class CustomerControllerAdapter {
         log.info("Respondiendo con los clientes");
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable String id) {
+        log.info("Se recibió una solicitud para obtener el cliente con ID: " + id);
+        Customer response = retriveCustomerByIdQuery.execute(id);
+        log.info("Respondiendo con el cliente");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
 }
