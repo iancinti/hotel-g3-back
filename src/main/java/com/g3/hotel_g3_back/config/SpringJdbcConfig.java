c3cad8cd2e83afb6e74eb7cce03bc342bf5b4747
Ian Cinti <ian.cinti@gmail.com>
2024-07-21 20:35:07 -0300

    add SpringJdbcConfig

---
+++ src/main/java/com/g3/hotel_g3_back/config/SpringJdbcConfig.java
@@ -0,0 +1,23 @@
+package com.g3.hotel_g3_back.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.g3.hotel_g3_back.config")
+public class SpringJdbcConfig {
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("");
+        dataSource.setUsername("");
+        dataSource.setPassword("");
+
+        return dataSource;
+    }
+}
