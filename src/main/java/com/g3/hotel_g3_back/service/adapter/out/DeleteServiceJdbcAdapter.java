7e88017d09841621b5a437688dc9c9abcaf39143
Ian Cinti <ian.cinti@gmail.com>
2024-07-21 02:37:55 -0300

    fix error

--- src/main/java/com/g3/hotel_g3_back/service/adapter/out/DeleteServiceJdbcAdapter.java
+++ src/main/java/com/g3/hotel_g3_back/service/adapter/out/DeleteServiceJdbcAdapter.java
@@ -1,12 +1,13 @@
 package com.g3.hotel_g3_back.service.adapter.out;
 
 import com.g3.hotel_g3_back.service.appliction.port.out.DeleteServiceRepository;
+import com.g3.hotel_g3_back.service.domain.Service;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class DeleteServiceJdbcAdapter implements DeleteServiceRepository {
     @Override
-    public void execute(String id) {
-        System.out.println("El servicio con " +id + " fue eliminado");
+    public void execute(String id, Service service) {
+        System.out.println("El servicio con ID " + id + " y detalles " + service + " fue eliminado");
     }
 }
