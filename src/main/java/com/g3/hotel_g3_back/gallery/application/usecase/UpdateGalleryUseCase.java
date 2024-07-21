7030d75faabf6788541448d98b142dddd4dab11e
Ian Cinti <ian.cinti@gmail.com>
2024-07-21 16:22:12 -0300

    fix error

--- src/main/java/com/g3/hotel_g3_back/gallery/application/usecase/UpdateGalleryUseCase.java
+++ src/main/java/com/g3/hotel_g3_back/gallery/application/usecase/UpdateGalleryUseCase.java
@@ -3,7 +3,6 @@ package com.g3.hotel_g3_back.gallery.application.usecase;
 import com.g3.hotel_g3_back.gallery.application.port.in.UpdateGalleryCommand;
 import com.g3.hotel_g3_back.gallery.application.port.out.UpdateGalleryRepository;
 import com.g3.hotel_g3_back.gallery.domain.Gallery;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
