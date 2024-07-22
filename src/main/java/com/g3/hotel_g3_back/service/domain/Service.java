4631451ae4d18d80d82a18cd7899c11dc4abb834
Ian Cinti <ian.cinti@gmail.com>
2024-07-21 21:13:09 -0300

    modif domain Service

--- src/main/java/com/g3/hotel_g3_back/service/domain/Service.java
+++ src/main/java/com/g3/hotel_g3_back/service/domain/Service.java
@@ -1,22 +1,23 @@
 package com.g3.hotel_g3_back.service.domain;
 
 public class Service {
-
-    private String id;
+    private int id;
     private String name;
-    private String date;
+    private String description;
+    private double price;
 
-    public Service(String id, String name, String date) {
+    public Service(int id, String name, String description, double price) {
         this.id = id;
         this.name = name;
-        this.date = date;
+        this.description = description;
+        this.price = price;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -28,11 +29,19 @@ public class Service {
         this.name = name;
     }
 
-    public String getDate() {
-        return date;
+    public String getDescription() {
+        return description;
     }
 
-    public void setDate(String date) {
-        this.date = date;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
