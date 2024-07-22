f25230c175e3936c27fbe5386676dd3460de9768
Ian Cinti <ian.cinti@gmail.com>
2024-07-21 23:33:11 -0300

    impl RetriveBookingByIdJdbcAdapter

--- src/main/java/com/g3/hotel_g3_back/booking/adapter/out/RetriveBookingByIdJdbcAdapter.java
+++ src/main/java/com/g3/hotel_g3_back/booking/adapter/out/RetriveBookingByIdJdbcAdapter.java
@@ -2,13 +2,45 @@ package com.g3.hotel_g3_back.booking.adapter.out;
 
 import com.g3.hotel_g3_back.booking.application.port.out.RetriveBookingByIdRepository;
 import com.g3.hotel_g3_back.booking.domain.Booking;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 public class RetriveBookingByIdJdbcAdapter implements RetriveBookingByIdRepository {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public RetriveBookingByIdJdbcAdapter(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private static final String SELECT_BOOKING_BY_ID_SQL = "SELECT id, id_customer, id_payment, check_in_date, check_out_date FROM Booking WHERE id = ?";
+
     @Override
-    public Booking execute(String id) {
-        return new Booking("1", "Cinti Ian", "14-07-2024");
+    public Booking execute(Integer id) {
+        try {
+            return jdbcTemplate.queryForObject(SELECT_BOOKING_BY_ID_SQL, new Object[]{id}, new BookingRowMapper());
+        } catch (DataAccessException e) {
+            System.out.println("Error al recuperar la reserva: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private static class BookingRowMapper implements RowMapper<Booking> {
+        @Override
+        public Booking mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Booking(
+                    rs.getInt("id"),
+                    rs.getInt("id_customer"),
+                    rs.getInt("id_payment"),
+                    rs.getDate("check_in_date"),
+                    rs.getDate("check_out_date")
+            );
+        }
     }
 }
--- src/main/java/com/g3/hotel_g3_back/booking/application/port/out/RetriveBookingByIdRepository.java
+++ src/main/java/com/g3/hotel_g3_back/booking/application/port/out/RetriveBookingByIdRepository.java
@@ -3,5 +3,5 @@ package com.g3.hotel_g3_back.booking.application.port.out;
 import com.g3.hotel_g3_back.booking.domain.Booking;
 
 public interface RetriveBookingByIdRepository {
-    Booking execute(String id);
+    Booking execute(Integer id);
 }
