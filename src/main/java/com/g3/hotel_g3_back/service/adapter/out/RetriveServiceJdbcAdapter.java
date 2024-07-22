66084e0ec78d039bd91e3b19b8fcb9b37fc91ba6
Ian Cinti <ian.cinti@gmail.com>
2024-07-21 21:09:35 -0300

    impl RetriveServiceJdbcAdapter

--- src/main/java/com/g3/hotel_g3_back/service/adapter/out/RetriveServiceJdbcAdapter.java
+++ src/main/java/com/g3/hotel_g3_back/service/adapter/out/RetriveServiceJdbcAdapter.java
@@ -2,20 +2,45 @@ package com.g3.hotel_g3_back.service.adapter.out;
 
 import com.g3.hotel_g3_back.service.appliction.port.out.RetiveServiceQueryRepository;
 import com.g3.hotel_g3_back.service.domain.Service;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
 public class RetriveServiceJdbcAdapter implements RetiveServiceQueryRepository {
 
-    @Override
-    public List<Service> execute(){
-        return Arrays.asList(
-                new Service("1","Wifi","Tod la estadia"),
-                new Service("2","Desayuno","Primer dia de estadia")
-        );
+    private final JdbcTemplate jdbcTemplate;
+
+    private static final String SELECT_ALL_SERVICES_SQL = "SELECT id, name, description, price FROM service";
+
+    public RetriveServiceJdbcAdapter(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
+    @Override
+    public List<Service> execute() {
+        try {
+            return jdbcTemplate.query(SELECT_ALL_SERVICES_SQL, new ServiceRowMapper());
+        } catch (DataAccessException e) {
+            System.out.println("Error al recuperar los servicios: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private static class ServiceRowMapper implements RowMapper<Service> {
+        @Override
+        public Service mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Service(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getString("description"),
+                    rs.getDouble("price")
+            );
+        }
+    }
 }
